ec9e418af8b4f33b425065fec22660cd0d71807f
Higor Wuttke Nunes <higor_wuttke_nunes@epam.com>
2024-12-04 22:42:36 +0100

    Added chat history + temperature setting

--- src/main/java/com/epam/training/gen/ai/controller/PromptController.java
+++ src/main/java/com/epam/training/gen/ai/controller/PromptController.java
@@ -2,6 +2,7 @@ package com.epam.training.gen.ai.controller;
 
 import com.epam.training.gen.ai.controller.dto.PromptRequest;
 import com.epam.training.gen.ai.controller.dto.PromptResponse;
+import com.epam.training.gen.ai.controller.dto.PromptResponseWithHistory;
 import com.epam.training.gen.ai.domain.PromptService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,13 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/prompt")
+@RequestMapping("/v1")
 public class PromptController {
     private final PromptService promptService;
 
-    @PostMapping
+    @PostMapping("/prompt")
     public PromptResponse processPrompt(@RequestBody PromptRequest request) {
         var promptCompletions = promptService.getPromptCompletions(request.input());
-        return new PromptResponse(promptCompletions.stream().findFirst().get());
+        return new PromptResponse(promptCompletions.stream().findFirst().orElse(""));
+    }
+
+    @PostMapping("/prompt-with-history")
+    public PromptResponseWithHistory processPromptWithHistory(@RequestBody PromptRequest request) {
+        var promptCompletions = promptService.getPromptCompletionsWithHistory(request.input());
+        return new PromptResponseWithHistory(promptCompletions);
     }
 }
--- src/main/java/com/epam/training/gen/ai/infra/SimplePromptService.java
+++ src/main/java/com/epam/training/gen/ai/infra/SimplePromptService.java
@@ -4,23 +4,41 @@ import com.azure.ai.openai.OpenAIAsyncClient;
 import com.azure.ai.openai.models.ChatCompletionsOptions;
 import com.azure.ai.openai.models.ChatRequestUserMessage;
 import com.epam.training.gen.ai.domain.PromptService;
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.orchestration.PromptExecutionSettings;
+import com.microsoft.semantickernel.semanticfunctions.KernelFunction;
+import com.microsoft.semantickernel.semanticfunctions.KernelFunctionArguments;
+import com.microsoft.semantickernel.services.chatcompletion.ChatHistory;
+import com.microsoft.semantickernel.services.chatcompletion.ChatMessageContent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @Service
 public class SimplePromptService implements PromptService {
     private final OpenAIAsyncClient aiAsyncClient;
     private final String deploymentName;
+    private final PromptExecutionSettings promptExecutionSettings;
+    private final Kernel kernel;
+    private final ChatHistory chatHistory;
 
     public SimplePromptService(OpenAIAsyncClient aiAsyncClient,
-                               @Value("${client-openai-deployment-name}") String deploymentName) {
+                               @Value("${client-openai-deployment-name}") String deploymentName,
+                               Map<String, PromptExecutionSettings> promptExecutionSettingsMap,
+                               Kernel kernel,
+                               @Value("${client-openai-execution-context}") String context) {
         this.aiAsyncClient = aiAsyncClient;
         this.deploymentName = deploymentName;
+        this.promptExecutionSettings = promptExecutionSettingsMap.get(deploymentName);
+        this.kernel = kernel;
+        this.chatHistory = new ChatHistory();
+        this.chatHistory.addSystemMessage(context);
     }
 
     @Override
@@ -28,13 +46,37 @@ public class SimplePromptService implements PromptService {
         var completions = aiAsyncClient
                 .getChatCompletions(
                         deploymentName,
-                        new ChatCompletionsOptions(
-                                List.of(new ChatRequestUserMessage(message))))
+                        new ChatCompletionsOptions(List.of(new ChatRequestUserMessage(message)))
+                                .setTemperature(promptExecutionSettings.getTemperature()))
                 .block();
-        var messages = completions.getChoices().stream()
+        var messages = Objects.requireNonNull(completions).getChoices().stream()
                 .map(c -> c.getMessage().getContent())
                 .toList();
         log.info(messages.toString());
         return messages;
+    }
+
+    @Override
+    public List<String> getPromptCompletionsWithHistory(String input) {
+        var response = kernel.invokeAsync(getChat())
+                .withArguments(getKernelFunctionArguments(input, chatHistory))
+                .block();
+        chatHistory.addUserMessage(input);
+        chatHistory.addAssistantMessage(Objects.requireNonNull(response).getResult());
+        return chatHistory.getMessages().stream().map(ChatMessageContent::getContent).toList();
+    }
+
+    private KernelFunction<String> getChat() {
+        return KernelFunction.<String>createFromPrompt("""
+                {{$chatHistory}}
+                <message role="user">{{$request}}""")
+                .build();
+    }
+
+    private KernelFunctionArguments getKernelFunctionArguments(String input, ChatHistory chatHistory) {
+        return KernelFunctionArguments.builder()
+                .withVariable("reqquest", input)
+                .withVariable("chatHistory", chatHistory)
+                .build();
     }
 }
