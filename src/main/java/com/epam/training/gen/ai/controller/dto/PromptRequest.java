03038e64694637e2fe914510f0a332f5a56bd7ae
Higor Wuttke Nunes <higor_wuttke_nunes@epam.com>
2024-12-12 13:54:51 +0100

    task 3

--- src/main/java/com/epam/training/gen/ai/controller/dto/PromptRequest.java
+++ src/main/java/com/epam/training/gen/ai/controller/dto/PromptRequest.java
@@ -1,4 +1,4 @@
 package com.epam.training.gen.ai.controller.dto;
 
-public record PromptRequest(String input) {
+public record PromptRequest(String input, String modelName, double temperature, boolean allowKernelFunctions) {
 }
